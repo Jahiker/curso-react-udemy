25fca5424f26bb4cf882d1113b80ae4c506317fe
jahiker <rojasjahiker@gmail.com>
2022-11-20 00:59:40 -0400

    test on async functions - promises

--- counter-app/test/base-pruebas/09-promesas.test.js
+++ counter-app/test/base-pruebas/09-promesas.test.js
@@ -1,10 +1,29 @@
-import { getHeroeByIdAsync } from "../../src/base-pruebas/09-promesas"
+import { getHeroeByIdAsync } from "../../src/base-pruebas/09-promesas";
 
-describe('Test on 09-promesas.js', () => {
-    test('getHeroeByIdAsync should return a heroe', () => {
+describe("Test on 09-promesas.js", () => {
+  test("getHeroeByIdAsync should return a heroe", (done) => {
+    const id = 1;
 
-        
+    getHeroeByIdAsync(id).then((hero) => {
+      expect(hero).toEqual({
+        id: 1,
+        name: "Batman",
+        owner: "DC",
+      });
+      done();
+    });
+  });
 
+  test("getHeroeByIdAsync should thorw an error if hero doesn't exist", (done) => {
+    const id = 100;
 
+    getHeroeByIdAsync(id)
+    .then(hero => {
+        expect( hero ).toBeFalsy();
     })
-})
+    .catch((error) => {
+      expect( error ).toBe("No se pudos encontrar el héroe");
+      done();
+    });
+  });
+});
