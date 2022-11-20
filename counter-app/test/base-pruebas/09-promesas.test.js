64b5ceccccb5727ae5b9157a698549a85306352a
jahiker <rojasjahiker@gmail.com>
2022-11-19 23:13:03 -0400

    async functions test

--- counter-app/test/base-pruebas/09-promesas.test.js
+++ counter-app/test/base-pruebas/09-promesas.test.js
@@ -1,5 +1,10 @@
+import { getHeroeByIdAsync } from "../../src/base-pruebas/09-promesas"
+
 describe('Test on 09-promesas.js', () => {
     test('getHeroeByIdAsync should return a heroe', () => {
+
+        
+
 
     })
 })
