f95489e86b6d547c52b15a1e5a62ce78240ff923
jahiker <rojasjahiker@gmail.com>
2022-11-30 23:36:04 -0400

    testing firstApp render

--- counter-app/test/FirstApp.test.jsx
+++ counter-app/test/FirstApp.test.jsx
@@ -2,8 +2,19 @@ import { render } from "@testing-library/react";
 import { FirstApp } from "../src/FirstApp";
 
 describe('Test on <FirstApp />', () => {
-    test('should match with snapshot', () => {
-        const title = "Esto es un titulo"
-        render( <FirstApp title={title} /> )
+    test('should match with the snapshot', () => {
+        const title = "Esto es un titulo";
+        const { container } = render( <FirstApp title={title} /> );
+
+        expect( container ).toMatchSnapshot();
+    })
+
+    test('should show the title on H1', () => {
+        const title = "Esto es un titulo";
+        const { container, getByText } = render( <FirstApp title={title} /> );
+
+        expect( getByText(title) ).toBeTruthy();
+
+
     })
 })
